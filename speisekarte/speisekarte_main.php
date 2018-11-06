473c6080962012a2d7a19019f8b2424151d293e6
Michael Mai <mm@mai-internet.de>
2018-11-06 08:47:41 +0100

    change main speisekarte

--- speisekarte/speisekarte_main.php
+++ speisekarte/speisekarte_main.php
@@ -15,8 +15,8 @@
             <td>21,50 €</td>
         </tr>
         <tr>
-            <td>Schnüsch – die Suppe aus der Nachbarschaft Angeln mit Gartengemüse und Schinken oder Matjes</td>
-            <td>8,50 €</td>
+            <td>Steckrüben Süpp’chen mit Speckcroutons</td>
+            <td>6,50 €</td>
         </tr>
         <tr>
             <td>
@@ -26,7 +26,7 @@
             </td>
             <td>19,50 €</td>
         </tr>
-<!--        -->
+        <!--        -->
         <tr class="grp">
             <td colspan="2"> Beef Tatar1867</td>
         </tr>
@@ -38,7 +38,7 @@
             <td>… als Hauptgericht</td>
             <td>21,50 €</td>
         </tr>
-<!--        -->
+        <!--        -->
         <tr>
             <td>
                 Ackersalat
@@ -49,17 +49,21 @@
                 9,50 €
             </td>
         </tr>
-        <tr>
-            <td>
-                Abendbrot
-                Wurscht und Käse, geräucherter Fisch,
-                dunkles Brot und gute Butter (auch am Mittag)
-
-            </td>
-            <td>
-                16,50 €
+        <!--        -->
+        <tr class="grp">
+            <td colspan="2">Foie gras<br>
+                kurz angebraten mit Zwiebel/Apfel Confit und getoastetem Brioche
             </td>
         </tr>
+        <tr class="grp">
+            <td> … als Vorspeise</td>
+            <td>16,50 €</td>
+        </tr>
+        <tr class="grp last">
+            <td>… als Hauptgericht</td>
+            <td>21,50 €</td>
+        </tr>
+        <!--        -->
         <tr class="grp">
             <td colspan="2">Knusprige Bratkartoffeln und Salat</td>
         </tr>
@@ -79,18 +83,7 @@
             <td>… vegan mit Zitronenpfeffer, Avocado-Dip, hausgemachtem Curry-Ketchup</td>
             <td>16,50 €</td>
         </tr>
-
-        <tr>
-            <td>
-                Labskaus
-                = Lob’s Course mit gepökelter Rinderbrust,
-                Spiegelei und Rollmops
-
-            </td>
-            <td>
-                21,50 €
-            </td>
-        </tr>
+        <!--        -->
         <tr>
             <td>
                 Steak (Entrecôte)
@@ -101,6 +94,7 @@
                 24,50 €
             </td>
         </tr>
+        <!--        -->
         <tr>
             <td>
                 Kalbsschnitzel
@@ -111,14 +105,59 @@
                 19,50 €
             </td>
         </tr>
+        <!--        -->
         <tr>
             <td>
-                …vegan
-                Bulgur, Aubergine und Yoghurt-Minz-Sauce
+                Abendbrot
+                Wurscht und Käse, geräucherter Fisch,
+                dunkles Brot und gute Butter (auch am Mittag)
+
             </td>
             <td>
                 16,50 €
             </td>
+        </tr>
+        <!--        -->
+        <tr>
+            <td>
+                …vegetarisch
+                Gemüse Tarte Tatin mit unserem winterlichen Salat
+            </td>
+            <td>
+                16,50 €
+            </td>
+        </tr>
+        <!--        -->
+        <tr>
+            <td>
+                Ganze Landente für Zwei
+                Serviettenknödel hausgemacht, Rot- und Rosenkohl, Orangensauce
+            <td>
+                49,00 €
+            </td>
+        </tr>
+        <!--        -->
+        <tr>
+            <td>
+                Schmorbraten vom Damwild aus Grünholz
+                Röstgemüse mit Kürbis und Maronen, Anna Kartoffeln
+            <td>
+                23,50 €
+            </td>
+        </tr>
+        <!--        -->
+        <tr class="grp">
+            <td colspan="2">Himmel und Erde
+                gebratene Blutwurst auf Kartoffelstampf, Schmoräpfel
+            </td>
+        </tr>
+        <tr class="grp">
+            <td> … als Vorspeise</td>
+            <td>9,50 €</td>
+        </tr>
+        <tr class="grp last">
+            <td>… als Hauptgericht</td>
+            <td>17,50 €</td>
         </tr>
         </tbody>
     </table>
