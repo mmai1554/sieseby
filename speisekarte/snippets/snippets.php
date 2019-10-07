a05aa5c7c33964449992fee7a83471ad89c631b1
Michael Mai <mm@mai-internet.de>
2019-10-07 10:17:24 +0200

    Container fuer bisherige Angebote auf der Speisekarte, damit man diese schnell wiederverwenden kann.

--- speisekarte/snippets/snippets.php
+++ speisekarte/snippets/snippets.php
@@ -1,1 +1,36 @@
 <?php
+die('not for you!');
+?>
+<div>
+    <h4>Schnüsch</h4>
+    <ul>
+        <li>Leichte Angeliter Gemüsesuppe mit Schinken oder Matjes: <span>8,50 €</span></li>
+    </ul>
+</div>
+
+<!--    -->
+<div>
+	<h4>Schmankerle für zwei</h4>
+	<ul>
+		<li>(natürlich auch möglich für eine Person)<br>
+			sechs kleine Vorspeisen von Hülsenfrüchten und aus sommerlichen Gemüsegarten: <span>21,50 €</span></li>
+	</ul>
+</div>
+
+
+<!--    -->
+<div>
+	<h4>Zarter Matjes</h4>
+	<ul>
+		<li>auf dünnen Scheiben von schwarzem Rettich und Birne: <span>14,50 €</span></li>
+	</ul>
+</div>
+
+
+<!--    -->
+<div>
+	<h4>Steak (Entrecôte)</h4>
+	<ul>
+		<li>von der Holsteiner Färse mit unseren Pommes und Salat: <span>24,50 €</span></li>
+	</ul>
+</div>
